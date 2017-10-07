b56752b2733af9eee81ffbb447f8f6fa00924127
guilh <guilh@DESKTOP-50P0AGP>
2017-10-07 10:24:54 -0300

    Classe

---
+++ src/lista/exercicios/srp/violation/Atendente.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package lista.exercicios.srp.violation;
+
+/**
+ * @author guilh
+ *
+ */
+public class Atendente {
+
+	public void reabastecimento(Veiculo v) {
+		v.setQuantidadeCombustivel(v.getCapacidadeTanqueCombustivel());
+	}
+}
